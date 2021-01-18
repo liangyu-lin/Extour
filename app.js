60ea5a87ca3a2eaf6dbbc820079a507537c7072f
LiangYu <liangyulin0727@gmail.com>
2021-01-18 15:04:02 -0800

    add start script

--- app.js
+++ app.js
@@ -130,8 +130,9 @@ app.use((err, req, res, next) => {
 });
 
 
-app.listen(3000, () => {
-    console.log("listen on port 3000")
+const port =  process.env.PORT || 3000;
+app.listen(port, () => {
+    console.log(`listen on port ${port}`)
 });
 
 
